ec240fbb0b1f3f903b279584318d08778e1bb0df
FKholbutayev <ht.uzpharma@gmail.com>
2021-07-31 19:54:18 +0300

    tests modified and fixed

--- api/app/Http/Controllers/API/UsersController.php
+++ api/app/Http/Controllers/API/UsersController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function show()
+    public function __construct()
     {
-
+        $this->middleware('auth:sanctum')->only('show');
+    }
+    public function show(Request $request)
+    {
+        return response()->json($request->user());
     }
 }
